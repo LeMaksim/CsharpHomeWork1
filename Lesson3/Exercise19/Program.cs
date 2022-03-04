bf2bc3fe9e3bee05b4d295b43eec1df0f7975cf9
Levitskiy Maksim <levitskiy.md@gmail.com>
2022-03-04 19:18:46 +0300

    add exercise 19

---
+++ Lesson3/Exercise19/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.Clear();
+async void Palindrom(int number)
+{
+    string pal = Convert.ToString(number);
+    int size = pal.Length;
+    int count1 = 0;
+    int count2 = size;
+    while (count1 <= size / 2)
+    {
+        if (((Math.Truncate(number / Math.Pow(10, count2 - 1)) % 10)) != ((Math.Truncate(number / Math.Pow(10, count1)) % 10)))
+        {
+            Console.WriteLine($"{number} is not a palindrome");
+            break;
+        }
+
+        else if ((((Math.Truncate(number / Math.Pow(10, count2 - 1)) % 10)) == ((Math.Truncate(number / Math.Pow(10, count1)) % 10))) && (count1 <= size / 2) && (count2 - count1 > 1))
+        {
+            count1++;
+            count2--;
+        }
+        else
+        {
+            Console.WriteLine($"{number} is palindrome");
+            break;
+        }
+    }
+}
+int a = 14212, b = 12821, c = 23432;
+
+Palindrom(a);
+Palindrom(b);
+Palindrom(c);
--- Lesson3/Exercise21/Program.cs
+++ Lesson3/Exercise21/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    Console.Write($"Distance between A({x1}, {y1}, {z1}) and B({x2}, {y2}, {z2}) is ");
+    double dist = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+    Console.WriteLine(dist);
+    return dist;
+    
+}
+
+double xa1 = 3, ya1 = 6, za1 = 8, xb1 = 2, yb1 = 1, zb1 = -7, xa2 = 7, ya2 = -5, za2 = 0, xb2 = 1, yb2 = -1, zb2 = 9;
+
+Distance(xa1, ya1, za1, xb1, yb1, zb1);
+
+Distance(xa2, ya2, za2, xb2, yb2, zb2);
